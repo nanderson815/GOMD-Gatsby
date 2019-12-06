44b7db5e4f9e5f59ef806480a37bdfbb16adba32
Noah A <nanderson815@gmail.com>
2019-12-06 09:36:40 -0500

    wokring on getting connected to wordpress

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,23 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import AppBar from '@material-ui/core/AppBar'
+import Container from '@material-ui/core/Container'
 
 const Header = ({ siteTitle }) => (
   <AppBar>
-
-    <h1 style={{ margin: 5 }}>
-      <Link
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-        }}
-      >
-        {siteTitle}
-      </Link>
-    </h1>
+    <Container>
+      <h1 style={{ margin: 5 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </Container>
   </AppBar>
 )
 
