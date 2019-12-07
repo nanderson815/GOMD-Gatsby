51b05a63a85f18d4623cb0223b28aeabd40821ce
Noah A <nanderson815@gmail.com>
2019-12-07 11:13:23 -0500

    trying to get media to work;

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,49 +51,23 @@ module.exports = {
       options: {
         /*
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
-         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
          */
-        baseUrl: "georgiaonmydime.com",
-        // The protocol. This can be http or https.
-        protocol: "https",
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
+        baseUrl: `georgiaonmydime.com`,
+        protocol: `https`,
         hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
-        // This feature is untested for sites hosted on wordpress.com.
-        // Defaults to true.
         useACF: true,
-        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
-        // It can help you debug specific API Endpoints problems.
-        verboseOutput: true,
-        // Set how many pages are retrieved per API request.
-        perPage: 100,
-        // Set WP REST API routes whitelists
-        // and blacklists using glob patterns.
-        // Defaults to whitelist the routes shown
-        // in the example below.
-        // See: https://github.com/isaacs/minimatch
-        // Example:  `["/*/*/comments", "/yoast/**"]`
-        // ` will either include or exclude routes ending in `comments` and
-        // all routes that begin with `yoast` from fetch.
-        // Whitelisted routes using glob patterns
         includedRoutes: [
           "**/categories",
           "**/posts",
-          "**/listing",
           "**/pages",
-          "**/media",
+          "**/listing",
+          // "**/media",
           "**/tags",
           "**/taxonomies",
           "**/users",
         ],
-        // use a custom normalizer which is applied after the built-in ones.
-        normalizer: function ({ entities }) {
-          return entities
-        },
-      },
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
