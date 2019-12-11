457c298dfd4b98428ca5d172e938037e8c06ddfb
Noah A <nanderson815@gmail.com>
2019-12-10 21:18:46 -0500

    semantic ui complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Menu } from 'semantic-ui-react'
+import { Menu, Container } from 'semantic-ui-react'
 
 const Header = ({ siteTitle }) => (
   <Menu>
-    <Menu.Item>
-      <h1 style={{ margin: 5 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </Menu.Item>
+    <Container>
+      <Menu.Item>
+        <h1 style={{ margin: 5 }}>
+          <Link
+            to="/"
+            style={{
+              color: `black`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </Menu.Item>
+    </Container>
   </Menu>
 )
 
