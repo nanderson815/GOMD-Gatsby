14ba3db4394a0b696634792f972861f80c3af0d3
Noah A <nanderson815@gmail.com>
2019-12-11 20:09:36 -0500

    getting data setup from site.

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Select, Input } from 'semantic-ui-react'
+import { useHappyHourData } from '../hooks/happyHourData'
 
 const options = [
     { key: 'all', text: 'All', value: 'all' },
@@ -7,12 +8,18 @@ const options = [
     { key: 'happyHours', text: 'Happy Hours', value: 'happyHours' },
 ]
 
-const SearchBar = () => (
-    <Input type='text' placeholder='Search...' action>
-        <input />
-        <Select compact options={options} defaultValue='happyHours' />
-        <Button primary type='submit'>Search</Button>
-    </Input>
-)
+const SearchBar = () => {
+
+    const data = useHappyHourData()
+    console.log(data);
+
+    return (
+        <Input type='text' placeholder='Search...' action>
+            <input />
+            <Select compact options={options} defaultValue='happyHours' />
+            <Button primary type='submit'>Search</Button>
+        </Input>
+    )
+}
 
 export default SearchBar
