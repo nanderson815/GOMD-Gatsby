cae0915466aac3871460a94e1bbf1b73d5c95537
Noah A <nanderson815@gmail.com>
2019-12-12 15:42:50 -0500

    search results displaying

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Input, Popup, List, Label } from 'semantic-ui-react'
 import { useHappyHourData } from '../hooks/happyHourData'
 import { Icon } from 'semantic-ui-react'
 import * as JsSearch from 'js-search'
@@ -10,6 +10,7 @@ const SearchBar = () => {
         happyHours: [],
         search: [],
         searchResults: [],
+        nameResults: [],
         isLoading: true,
         isError: false,
     })
@@ -31,18 +32,44 @@ const SearchBar = () => {
         const { search } = state
         if (e.target.value !== "") {
             const queryResult = search.search(e.target.value)
-            setState({ ...state, searchResults: queryResult })
+            const names = queryResult.map(item => item.name)
+            setState({ ...state, searchResults: queryResult, nameResults: names })
             console.log(state.searchResults);
+        } else {
+            setState({ ...state, searchResults: [], nameResults: [] })
         }
 
     }
 
     return (
-        <Input type='text' placeholder='Search restaurants, happy hours, etc...' action>
-            <input
-                onChange={searchData} />
-            <Button primary type='submit'> <Icon name='search' style={{ marginLeft: "5px" }} /></Button>
-        </Input>
+        <Popup
+            wide
+            position='bottom left'
+            style={{ marginTop: "auto" }}
+            open={!(state.nameResults === undefined || state.nameResults.length === 0)}
+            trigger={
+                <Input type='text' placeholder='Search restaurants, happy hours, etc...' action>
+                    <input
+                        onChange={searchData}
+                    />
+                    <Button primary type='submit'> <Icon name='search' style={{ marginLeft: "5px" }} /></Button>
+                </Input>}>
+            <Popup.Header>
+                <List divided>
+                    {state.searchResults ? state.searchResults.slice(0, 5).map((item) => {
+                        return (
+                            <List.Item key={item.id}>
+                                <List.Content>
+                                    <List.Header>{item.name}</List.Header>
+                                    {/* <List.Description style={{ fontSize: "12px" }}>{item.tags.toString()}</List.Description> */}
+                                    {item.tags.map((tag, index) => <Label key={index}>{tag}</Label>)}
+                                </List.Content>
+                            </List.Item>
+                        )
+                    }) : null}
+                </List>
+            </Popup.Header>
+        </Popup >
     )
 }
 
