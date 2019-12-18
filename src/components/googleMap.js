911f8a3e8d505bb0c242032a62cb1e72d8d32ec7
Noah A <nanderson815@gmail.com>
2019-12-17 20:34:51 -0500

    adding points dynamically on map

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,51 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Icon } from 'semantic-ui-react';
 
 const Marker = ({ info }) => <Icon size="huge" name="map marker alternate" style={{ color: "#1c70b5" }} />;
 
-let locations = [
-    { lat: 33.78575, lng: -84.3844 },
-    { lat: 33.78575, lng: -84.4112 }
-]
 
-const markers = locations.map((m, i) => {
+
+
+const SimpleMap = ({ happyHours }) => {
+    const center = { lat: 33.78454, lng: -84.3880 }
+    const zoom = 11
+    const [markers, setMarkers] = React.useState([]);
+
+    useEffect(() => {
+        if (happyHours.length > 0) {
+            console.log(happyHours)
+            let points = happyHours.map((m, i) => {
+                return (
+                    <Marker
+                        key={i}
+                        lat={m.location.lat}
+                        lng={m.location.lon}
+                    />
+                )
+            });
+            setMarkers(points);
+        }
+    }, [happyHours])
+
+
     return (
-        <Marker
-            key={i}
-            lat={m.lat}
-            lng={m.lng}
-        />
-    )
-});
-
-
-class SimpleMap extends Component {
-    static defaultProps = {
-        center: {
-            lat: 33.78454,
-            lng: -84.3880
-        },
-        zoom: 11
-    };
-
-    render() {
-
-        console.log(this.props);
-
-        return (
-            // Important! Always set the container height explicitly as less than 100vh to avoid strange sticky behavior.
-            <div style={{ height: '99.99vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyC-3IzXoP2LU2QhnrfuhZWHp_QGedI7VdY" }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    {markers}
-                </GoogleMapReact>
-            </div>
-        );
-    }
+        // Important! Always set the container height explicitly as less than 100vh to avoid strange sticky behavior.
+        <div style={{ height: '99.99vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyC-3IzXoP2LU2QhnrfuhZWHp_QGedI7VdY" }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+            >
+                {markers}
+            </GoogleMapReact>
+        </div>
+    );
 }
 
 export default SimpleMap;
