42bc82df8a008cc600cd744357d1bf2f7545ca79
Noah A <nanderson815@gmail.com>
2019-12-22 19:09:04 -0500

    changed static map to imbed

--- src/Templates/happyHour.js
+++ src/Templates/happyHour.js
@@ -84,13 +84,19 @@ const HappyHour = (props) => {
             <Sticky>
               <Card fluid raised>
                 <a
-                  style={{ marginBottom: '-18px' }}
+                  style={{ overflow: 'hidden', height: "225px" }}
                   href={`https://www.google.com/maps/search/?api=1&query=${post.address}`}>
-                  <img alt="resetaurant location map" style={{ marginBottom: "-5px" }} src={`https://maps.googleapis.com/maps/api/staticmap?center=${post.location.lat},${post.location.lon}&markers=color:0x1c70b5%7C${post.location.lat},${post.location.lon}&zoom=15&size=500x268&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`} />
+                  {/* <img alt="resetaurant location map" style={{ marginBottom: "-5px" }} src={`https://maps.googleapis.com/maps/api/staticmap?center=${post.location.lat},${post.location.lon}&markers=color:0x1c70b5%7C${post.location.lat},${post.location.lon}&zoom=15&size=500x268&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`} /> */}
+                  <iframe width="100%" height="240px" frameBorder="0" style={{ border: "0", margin: "0px" }} src={`https://www.google.com/maps/embed/v1/place?q=${post.address}&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`}></iframe>
                 </a>
                 <Card.Content style={{ background: "white" }}>
                   <Card.Description>
-                    {post.address}
+                    <a
+                      style={{ color: "grey" }}
+                      href={`https://www.google.com/maps/search/?api=1&query=${post.address}`}>
+                      {post.address}
+
+                    </a>
                   </Card.Description>
                   <Card.Description>
                     {`${formatPhoneNumber(post.phone)} |`} <a style={{ color: "#1c70b5", fontWeight: "bold" }} href={post.website}>Website</a>
