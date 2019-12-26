5ea35e4e3afdef5308a88140a0ca22fff95b9dd6
Noah A <nanderson815@gmail.com>
2019-12-26 18:11:23 -0500

    trying again without window ref

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from '../components/header'
 import SEO from "../components/seo"
 import 'semantic-ui-less/semantic.less'
@@ -10,6 +10,13 @@ import BackgroundImage from 'gatsby-background-image'
 
 
 const IndexPage = ({ data }) => {
+
+  const [screen, setScreen] = React.useState('');
+
+  useEffect(() => {
+    let screen = window ? window.screen.width : 1000;
+    setScreen(screen);
+  }, [])
 
   let happyHours = data.allContentfulHappyHour.edges.map(item => item.node)
   console.log(happyHours)
@@ -50,7 +57,7 @@ const IndexPage = ({ data }) => {
 
       let width;
 
-      if (window.screen.width < 768) {
+      if (screen < 768) {
         width = "100%"
       } else {
         width = index % 3 === 0 ? "65%" : "30%"
@@ -65,7 +72,7 @@ const IndexPage = ({ data }) => {
           <BackgroundImage
             fluid={[`linear-gradient(rgba(225, 225, 225, 0.2), rgba(0, 0, 0, 0.9))`, item.mainImg.fluid]}
             alt={item.name}
-            style={{ height: `${window.screen.width < 768 ? "20vh" : "50vh"}` }}
+            style={{ height: "50vh" }}
           >
             <h2 style={{ color: "white", position: "absolute", bottom: "20px", left: "10px", zIndex: "2000" }}>{item.name}</h2>
           </BackgroundImage>
@@ -81,33 +88,33 @@ const IndexPage = ({ data }) => {
       )
     })
 
-    // let segment = (
-    //   <Card link key={`mapKeyCardLink`} style={{ width: `${window.screen.width < 768 ? "100%" : "33%"}`, background: "#1c70b5" }}>
-    //     <Card.Content>
-    //       <Card.Header>
-    //         <h1 style={{ color: 'white', marginTop: "20px" }}>Looking to grab a beer or a bite?</h1>
-    //       </Card.Header>
-    //       <br></br>
-    //       <Card.Description>
-    //         <h2 style={{ color: "white" }}> We might have a couple of suggestions if you're into...</h2>
-    //         <ul style={{ color: "white", fontWeight: "bold", fontSize: "20px" }}>
-    //           <li>Sushi</li>
-    //           <li>Beer</li>
-    //           <li>Cocktails</li>
-    //           <li>Trying New Things</li>
-    //           <li>Tacos</li>
-    //           <li>Oysters</li>
-    //           <li>Saving Money</li>
-    //         </ul>
-    //         <br></br>
-    //       </Card.Description>
-    //       <Card.Description>
-    //         <Button onClick={() => navigate("/happy-hour-finder")} style={{ width: "100%", textAlign: "center", marginTop: '50px' }} secondary>Find Happy Hours <Icon name="arrow right"></Icon ></Button>
-    //       </Card.Description>
-    //     </Card.Content>
-    //   </Card>)
+    let segment = (
+      <Card link key={`mapKeyCardLink`} style={{ width: `${screen < 768 ? "100%" : '33%'}`, background: "#1c70b5" }}>
+        <Card.Content>
+          <Card.Header>
+            <h1 style={{ color: 'white', marginTop: "20px" }}>Looking to grab a beer or a bite?</h1>
+          </Card.Header>
+          <br></br>
+          <Card.Description>
+            <h2 style={{ color: "white" }}> We might have a couple of suggestions if you're into...</h2>
+            <ul style={{ color: "white", fontWeight: "bold", fontSize: "20px" }}>
+              <li>Sushi</li>
+              <li>Beer</li>
+              <li>Cocktails</li>
+              <li>Trying New Things</li>
+              <li>Tacos</li>
+              <li>Oysters</li>
+              <li>Saving Money</li>
+            </ul>
+            <br></br>
+          </Card.Description>
+          <Card.Description>
+            <Button onClick={() => navigate("/happy-hour-finder")} style={{ width: "100%", textAlign: "center", marginTop: '50px' }} secondary>Find Happy Hours <Icon name="arrow right"></Icon ></Button>
+          </Card.Description>
+        </Card.Content>
+      </Card>)
 
-    // cards.splice(6, 0, segment)
+    cards.splice(6, 0, segment)
     return cards
   }
 
