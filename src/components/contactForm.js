88250ca1db81475b236aaef1e6f89df34eaf8c4c
Noah A <nanderson815@gmail.com>
2020-01-01 20:05:48 -0500

    adding contact form and page

---
+++ src/components/contactForm.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
+
+const FormExampleFieldControlId = () => (
+    <Form>
+        <Form.Group widths='equal'>
+            <Form.Field
+                id='form-input-control-first-name'
+                control={Input}
+                label='First name'
+                placeholder='First name'
+            />
+            <Form.Field
+                id='form-input-control-last-name'
+                control={Input}
+                label='Last name'
+                placeholder='Last name'
+            />
+        </Form.Group>
+        <Form.Field
+            id='form-textarea-control-message'
+            control={TextArea}
+            label='Message'
+            placeholder='Message'
+        />
+        <Form.Field
+            id='form-input-control-error-email'
+            control={Input}
+            label='Email'
+            // error={{
+            //     content: 'Please enter a valid email address',
+            //     pointing: 'below',
+            // }}
+            error={false}
+        />
+        <Form.Field
+            id='form-button-control-public'
+            control={Button}
+            content='Confirm'
+        />
+    </Form>
+)
+
+export default FormExampleFieldControlId
---
+++ src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Header from '../components/header'
+import SEO from '../components/seo'
+import { Grid, Responsive } from 'semantic-ui-react'
+import ContactForm from '../components/contactForm'
+
+const Contact = () => {
+
+    return (
+        <div>
+            <SEO
+                title="Contact Us"></SEO>
+            <Header></Header>
+            <Layout>
+                <Grid style={{ minHeight: "80vh" }} stackable columns={2}>
+                    <Grid.Column>
+                        <h1>Contact Us</h1>
+                        <ContactForm></ContactForm>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <iframe title="Happy Hour Map" width="100%" height="100vh" frameBorder="0" style={{ border: "0", margin: "0px", height: '100%' }} src={`https://www.google.com/maps/embed/v1/place?q=Atlanta,GA&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`}></iframe>
+                    </Grid.Column>
+                </Grid>
+
+            </Layout>
+        </div>
+    )
+};
+
+export default Contact;
