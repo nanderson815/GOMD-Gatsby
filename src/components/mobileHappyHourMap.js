7f293c72b57b1c0876f516e18de587bf440aa6da
Noah A <nanderson815@gmail.com>
2020-01-12 17:50:07 -0500

    alerting visible element on scroll

--- src/components/mobileHappyHourMap.js
+++ src/components/mobileHappyHourMap.js
@@ -1,16 +1,43 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "semantic-ui-less/semantic.less"
 import GoogleMap from '../components/googleMap'
-import { Card } from "semantic-ui-react"
+import { Card, Icon } from "semantic-ui-react"
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
-
-
 import './layout.css'
 
+
+const handleIntersect = (entries, observer) => {
+    entries.forEach(entry => {
+        console.log(entry.target.id)
+    })
+}
+
+const createObserver = (elements) => {
+    let observer;
+    let options = {
+        root: document.getElementById("scrollArea"),
+        rootMargin: "0px",
+        threshold: 1.0
+    };
+    observer = new IntersectionObserver(handleIntersect, options);
+    elements.forEach(element => observer.observe(element))
+}
+
+
+
+
+
 const MobileHappyHourMap = ({ filteredHH, hovered }) => {
-    let length = filteredHH.length * 305
-    console.log(length);
+    useEffect(() => {
+        console.log(filteredHH)
+        let elements = []
+        filteredHH.forEach(card => elements.push(document.getElementById(`${card.id}`)))
+        createObserver(elements);
+    }, [filteredHH])
+
+
+    let width = filteredHH.length * 304 + 30
     return (
         <>
             <GoogleMap
@@ -23,16 +50,23 @@ const MobileHappyHourMap = ({ filteredHH, hovered }) => {
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}>
             </GoogleMap>
 
-            <div className="scrolling-snapper" style={{ position: "absolute", bottom: "20px", left: "10px", width: "100%" }}>
-                <Card.Group style={{ width: length }}>
-                    {filteredHH.map(card => {
+            <div id="scrollArea" className="scrolling-snapper" style={{ position: "absolute", bottom: "20px", width: "calc(100vw)" }}>
+                <Card.Group style={{ width: width }}>
+                    {filteredHH.map((card, index) => {
                         return (
-                            <Card className="child-snap" key={card.id}>
-                                <Link to={`/atlanta-happy-hour/${card.slug}`}>
-                                    <Img style={{ height: "100px" }} alt={card.name + ' Happy Hour atlanta'} fluid={card.mainImg.fluid} />
-                                </Link>
+                            <Card
+                                style={{ marginLeft: `${index === 0 ? "20px" : null}` }}
+                                className="child-snap"
+                                key={card.id}
+                                id={card.id}>
+                                <Img style={{ height: "80px" }} alt={card.name + ' Happy Hour atlanta'} fluid={card.mainImg.fluid} />
                                 <Card.Content>
-                                    <Card.Header>{card.name}</Card.Header>
+                                    <Card.Header style={{ marginBottom: "-15px" }}>{card.name}</Card.Header>
+                                </Card.Content>
+                                <Card.Content style={{ padding: "3px 0px 0px 5px", marginBottom: "-5px" }}>
+                                    <Link to={`/atlanta-happy-hour/${card.slug}`}>
+                                        <Card.Description style={{ color: "grey" }}>Show Happy Hours <Icon style={{ float: "right" }} name="chevron right"></Icon> </Card.Description>
+                                    </Link>
                                 </Card.Content>
                             </Card>
                         )
