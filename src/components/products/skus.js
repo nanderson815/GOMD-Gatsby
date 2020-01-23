c43fb0ff9842e1c48a0751c41b019e81b5fd5d56
Noah A <nanderson815@gmail.com>
2020-01-23 05:56:54 -0500

    getting sku data with a hook

--- src/components/products/skus.js
+++ src/components/products/skus.js
@@ -1,25 +1,16 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { useStripeSkuData } from '../../hooks/skuData'
 
-export default props => (
-    <StaticQuery
-        query={graphql`
-      query SkusForProduct {
-        skus: allStripeSku {
-          edges {
-            node {
-              ...allSkuFields
-            }
-          }
-        }
-      }
-    `}
-        render={({ skus }) => (
-            <div>
-                {skus.edges.map(({ node: sku }) => (
-                    <p key={sku.id}>{sku.product.name}</p>
-                ))}
-            </div>
-        )}
-    />
-)
+
+const Skus = (props) => {
+    const skus = useStripeSkuData();
+    console.log(skus)
+
+    return (
+        <div>
+            Skus will go here.
+        </div>
+    )
+}
+
+export default Skus;
---
+++ src/hooks/skuData.js
@@ -0,0 +1,19 @@
+import { graphql, useStaticQuery } from "gatsby"
+export const useStripeSkuData = () => {
+    const { allStripeSku } = useStaticQuery(
+        graphql`
+    query SkusForProduct {
+        allStripeSku {
+          edges {
+            node {
+              ...allSkuFields
+            }
+          }
+        }
+      }
+    `
+    )
+    return allStripeSku.edges.map(item => item.node)
+}
+
+
