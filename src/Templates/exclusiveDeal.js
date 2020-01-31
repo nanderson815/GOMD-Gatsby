57938d486df282a58ce93a150910fd95d9c7a199
Noah A <nanderson815@gmail.com>
2020-01-31 14:29:20 -0500

    adding new loading wheel

--- src/Templates/exclusiveDeal.js
+++ src/Templates/exclusiveDeal.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby';
 import Header from '../components/header'
 import Img from 'gatsby-image';
 import Layout from '../components/layout'
-import { Grid, Label, Card, Segment, Button, Icon } from 'semantic-ui-react'
+import { Grid, Label, Card, Segment, Button, Icon, Dimmer, Loader } from 'semantic-ui-react'
 import SEO from '../components/seo'
 import AdSense from 'react-adsense';
 import Axios from 'axios';
@@ -25,24 +25,31 @@ const ExclusiveDeal = (props) => {
   const post = get(props, 'data.stripeSku')
   console.log(post);
 
-
-  let [voucher, setVoucher] = useState()
+  const [pageLoading, setPageLoading] = useState(true)
+  const [voucher, setVoucher] = useState()
   useEffect(() => {
     let db = getFirebase().firestore()
     db.collection('vouchers').doc(post.product.id).get().then((doc) => {
       console.log(doc.data())
       setVoucher(doc.data())
+      setPageLoading(false)
     })
   }, [])
 
   const [loading, setLoading] = useState(false)
   const handleCheckout = (name, desc, price, image) => {
     setLoading(true);
+    let redirect = '';
+    if (typeof window !== "undefined") {
+      redirect = window.location.origin
+    } else {
+      redirect = "http://localhost:8000/"
+    }
     let user = getUser()
     if (user) {
       let data = {
-        successUrl: "http://localhost:8000/app/profile",
-        cancelUrl: `http://localhost:8000/exclusive-dining/${post.product.metadata.slug}`,
+        successUrl: `${redirect}/app/profile`,
+        cancelUrl: `${redirect}/exclusive-dining/${post.product.metadata.slug}`,
         name: name,
         image: image,
         description: desc,
@@ -94,74 +101,76 @@ const ExclusiveDeal = (props) => {
         image={post.product.localFiles[0].childImageSharp.fluid.src}
       />
       <Layout>
-        <Grid>
-          <Grid.Column computer={11} tablet={11} mobile={16}>
-            <Card fluid raised>
-              <Img style={{ maxHeight: "350px" }} alt={post.product.name} fluid={post.product.localFiles[0].childImageSharp.fluid} />
-              <Card.Content>
-                <Card.Header>
-                  <h1 style={{ marginBottom: "-3px" }}>{post.product.name}</h1>
-                  {post.product.attributes.map((tag, index) => <Label as={Button} key={`${index}label`} value={tag} onClick={handleClick}>{tag}</Label>)}
-                  <div style={{ display: "inline-block", float: 'right', fontSize: "25px" }}>
-                    {formatCurrency(post.price)}
+        {pageLoading ? <Dimmer active inverted>
+          <Loader inverted>Loading</Loader>
+        </Dimmer> : <Grid>
+            <Grid.Column computer={11} tablet={11} mobile={16}>
+              <Card fluid raised>
+                <Img style={{ maxHeight: "350px" }} alt={post.product.name} fluid={post.product.localFiles[0].childImageSharp.fluid} />
+                <Card.Content>
+                  <Card.Header>
+                    <h1 style={{ marginBottom: "-3px" }}>{post.product.name}</h1>
+                    {post.product.attributes.map((tag, index) => <Label as={Button} key={`${index}label`} value={tag} onClick={handleClick}>{tag}</Label>)}
+                    <div style={{ display: "inline-block", float: 'right', fontSize: "25px" }}>
+                      {formatCurrency(post.price)}
+                    </div>
+                  </Card.Header>
+
+                </Card.Content>
+                <Card.Content>
+                  <h2>Description</h2>
+                  {post.product.description.split("\\n").map((item, i) => <p key={i}>{item}</p>)}
+                </Card.Content>
+                <Card.Content>
+                  <Icon name="map marker"></Icon> {voucher ? voucher.neighborhood : null}
+                  <div style={{ display: 'inline-block', float: "right", fontWeight: "bold", fontSize: "20px" }}>
+                    Remaining: {voucher ? checkRemaining(voucher.vouchersSold, voucher.quantity) : null}
                   </div>
-                </Card.Header>
+                </Card.Content>
 
-              </Card.Content>
-              <Card.Content>
-                <h2>Description</h2>
-                <p>{post.product.description}</p>
-              </Card.Content>
-              <Card.Content>
-                <Icon name="map marker"></Icon> {voucher ? voucher.neighborhood : null}
-                <div style={{ display: 'inline-block', float: "right", fontWeight: "bold", fontSize: "20px" }}>
-                  Remaining: {voucher ? checkRemaining(voucher.vouchersSold, voucher.quantity) : null}
+              </Card>
+            </Grid.Column>
+            <Grid.Column tablet={5} computer={5} mobile={16}>
+              <Card fluid raised>
+                <div
+                  style={{ overflow: 'hidden', height: "225px" }}
+                >
+                  <iframe title="Happy Hour Map" width="100%" height="240px" frameBorder="0" style={{ border: "0", margin: "0px" }} src={`https://www.google.com/maps/embed/v1/place?q=${post.product.metadata.address}&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`}></iframe>
                 </div>
-              </Card.Content>
+                <Card.Content style={{ background: "white" }}>
+                  <Card.Description>
+                    <a
+                      style={{ color: "grey" }}
+                      href={`https://www.google.com/maps/search/?api=1&query=${post.product.metadata.address}`}>
+                      {post.product.metadata.address}
+                    </a>
+                  </Card.Description>
 
-            </Card>
-          </Grid.Column>
-          <Grid.Column tablet={5} computer={5} mobile={16}>
-            <Card fluid raised>
-              <div
-                style={{ overflow: 'hidden', height: "225px" }}
-              >
-                <iframe title="Happy Hour Map" width="100%" height="240px" frameBorder="0" style={{ border: "0", margin: "0px" }} src={`https://www.google.com/maps/embed/v1/place?q=${post.product.metadata.address}&key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`}></iframe>
-              </div>
-              <Card.Content style={{ background: "white" }}>
-                <Card.Description>
-                  <a
-                    style={{ color: "grey" }}
-                    href={`https://www.google.com/maps/search/?api=1&query=${post.product.metadata.address}`}>
-                    {post.product.metadata.address}
-                  </a>
-                </Card.Description>
-
-              </Card.Content>
-            </Card>
-            {voucher ? voucher.vouchersSold >= voucher.quantity ? null : <Segment raised style={{ paddingBottom: '1px', textAlign: "center" }}>
-              <p>{post.product.caption}</p>
-              <Button
-                primary
-                loading={loading}
-                disabled={loading}
-                style={{ marginBottom: '15px' }}
-                onClick={() => handleCheckout(post.product.name, post.product.description, post.price, post.product.images)}>
-                BUY NOW
+                </Card.Content>
+              </Card>
+              {voucher ? voucher.vouchersSold >= voucher.quantity ? null : <Segment raised style={{ paddingBottom: '1px', textAlign: "center" }}>
+                <p>{post.product.caption}</p>
+                <Button
+                  primary
+                  loading={loading}
+                  disabled={loading}
+                  style={{ marginBottom: '15px' }}
+                  onClick={() => handleCheckout(post.product.name, post.product.description, post.price, post.product.images)}>
+                  BUY NOW
               </Button>
-            </Segment> : null}
-            <Segment raised style={{ paddingBottom: '1px' }}>
-              <AdSense.Google
-                client="ca-pub-4839737207231731"
-                slot='4063925755'
-                responsive='true'
-                format='auto'
-                style={{ display: 'block', width: "100% !important" }}
-              />
-              <p style={{ width: "100%", textAlign: 'center', fontSize: "12px" }}><i>sponsored content</i></p>
-            </Segment>
-          </Grid.Column>
-        </Grid>
+              </Segment> : null}
+              <Segment raised style={{ paddingBottom: '1px' }}>
+                <AdSense.Google
+                  client="ca-pub-4839737207231731"
+                  slot='4063925755'
+                  responsive='true'
+                  format='auto'
+                  style={{ display: 'block', width: "100% !important" }}
+                />
+                <p style={{ width: "100%", textAlign: 'center', fontSize: "12px" }}><i>sponsored content</i></p>
+              </Segment>
+            </Grid.Column>
+          </Grid>}
       </Layout >
     </>
   )
--- src/pages/exclusive-dining.js
+++ src/pages/exclusive-dining.js
@@ -5,13 +5,15 @@ import { Responsive } from 'semantic-ui-react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { getFirebase } from '../firebase/firebase'
-
+import { Dimmer, Loader } from 'semantic-ui-react'
 import Skus from "../components/Products/Skus"
 
 const ExclusiveDiningPage = () => {
 
+    const [loading, setLoading] = useState(true)
     const [vouchers, setVouchers] = useState([])
     useEffect(() => {
+        setLoading(true)
         let db = getFirebase().firestore()
         let vouchers = [];
         db.collection('vouchers').get()
@@ -19,9 +21,13 @@ const ExclusiveDiningPage = () => {
                 snap.forEach(doc => {
                     vouchers.push(doc.data())
                 })
+                setLoading(false)
                 setVouchers(vouchers)
             })
-            .catch(err => console.log(err))
+            .catch(err => {
+                setLoading(false)
+                console.log(err)
+            })
     }, [])
 
     return (
@@ -29,15 +35,21 @@ const ExclusiveDiningPage = () => {
             <SEO title="Exclusive Deals" />
             <Header></Header>
             <Layout>
-                <h1>Exclusive dining options at the best restaurants in Atlanta.</h1>
-                <div style={{ minHeight: '70vh' }}>
-                    <Responsive minWidth={768}>
-                        <Skus vouchers={vouchers} columns={3} />
-                    </Responsive>
-                    <Responsive {...Responsive.onlyMobile}>
-                        <Skus vouchers={vouchers} columns={1} />
-                    </Responsive>
-                </div>
+                {loading ? <Dimmer active inverted>
+                    <Loader inverted>Loading</Loader>
+                </Dimmer>
+                    :
+                    <div>
+                        <h1>Exclusive dining options at the best restaurants in Atlanta.</h1>
+                        <div style={{ minHeight: '70vh' }}>
+                            <Responsive minWidth={768}>
+                                <Skus vouchers={vouchers} columns={3} />
+                            </Responsive>
+                            <Responsive {...Responsive.onlyMobile}>
+                                <Skus vouchers={vouchers} columns={1} />
+                            </Responsive>
+                        </div>
+                    </div>}
             </Layout>
         </div>
     )
