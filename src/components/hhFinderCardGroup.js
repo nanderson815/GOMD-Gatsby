7acf1b39c746adde167dc87b0cb457191338ac0b
Noah Anderson <nanderson815@gmail.com>
2020-08-18 19:52:19 -0400

    fixed favorites

--- src/components/hhFinderCardGroup.js
+++ src/components/hhFinderCardGroup.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Icon } from 'semantic-ui-react'
 import Img from 'gatsby-image'
-import { Link } from 'gatsby'
+import { navigate } from 'gatsby'
 import AdSense from 'react-adsense'
 import { setHHTime } from '../Util/Util'
 
-// const navfunc = (e, { slug }) => {
-//     navigate("/atlanta-happy-hour/" + slug)
-// }
+const navfunc = slug => {
+  navigate(`/atlanta-happy-hour/${slug}`)
+}
 
 const HHFinderCardGroup = ({ happyHours, day, rows, handleStarClick, user, offset }) => {
   const [cards, setCards] = useState('')
@@ -25,28 +25,19 @@ const HHFinderCardGroup = ({ happyHours, day, rows, handleStarClick, user, offse
           // onMouseEnter={setHoverHandler.bind(this, deal.id)}
           // onMouseLeave={clearHoveredHandler}
           link
-          // onClick={navfunc}
+          onClick={() => navfunc(deal.slug)}
           key={deal.id}
           id={deal.id}
           slug={deal.slug}
         >
-          <Link to={`/atlanta-happy-hour/${deal.slug}`}>
-            <Img style={{ height: '150px' }} alt={`${deal.name} Happy Hour atlanta`} fluid={deal.mainImg.fluid} />
-          </Link>
+          <Img style={{ height: '150px' }} alt={`${deal.name} Happy Hour atlanta`} fluid={deal.mainImg.fluid} />
           <Card.Content>
-            <Card.Header style={{ marginBottom: '5px' }}>
-              {' '}
-              <Link style={{ color: 'black' }} to={`/atlanta-happy-hour/${deal.slug}`}>
-                {deal.name}
-              </Link>
-            </Card.Header>
-            <Link style={{ color: 'grey' }} to={`/atlanta-happy-hour/${deal.slug}`}>
-              {day === 'All' ? null : (
-                <Card.Description>
-                  {setHHTime(deal, timeField)} {trimmedString}
-                </Card.Description>
-              )}
-            </Link>
+            <Card.Header style={{ marginBottom: '5px' }}>{deal.name}</Card.Header>
+            {day === 'All' ? null : (
+              <Card.Description>
+                {setHHTime(deal, timeField)} {trimmedString}
+              </Card.Description>
+            )}
           </Card.Content>
           <Card.Content extra value={deal.neighborhood}>
             <Icon name='marker' />
