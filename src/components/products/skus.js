acd81426c298a6eaa5dfecaaec45ccf754ff2d81
Noah Anderson <nanderson815@gmail.com>
2021-06-01 21:18:38 -0400

    remove voucher based on display flag

--- src/components/products/skus.js
+++ src/components/products/skus.js
@@ -20,32 +20,34 @@ const Skus = ({ columns, vouchers }) => {
 
   return (
     <Card.Group itemsPerRow={columns}>
-      {merged.map(voucher => (
-        <Card link key={voucher.id} onClick={() => onClickHandler(voucher.product.metadata.slug)}>
-          {/* <Link to={`/exclusive-dining/${voucher.product.metadata.slug}`}> */}
-          <Img style={{ height: '200px' }} fluid={voucher.product.localFiles[0].childImageSharp.fluid} />
-          <Card.Content style={{ minHeight: '130px' }}>
-            <Label style={{ top: '-55px' }} color='blue' ribbon='right'>
-              Remaining: {checkRemaining(voucher.vouchersSold, voucher.quantity)}
-            </Label>
-            <Card.Header style={{ marginTop: '-30px', minHeight: '45px' }}>{voucher.name}</Card.Header>
-            <Card.Description style={{ minHeight: '38px' }}>{voucher.product.metadata.caption}</Card.Description>
-          </Card.Content>
-          <Card.Content>
-            <Grid>
-              <Grid.Column width={8}>
-                <h3 style={{ color: 'black' }}>{formatCurrency(voucher.price)}</h3>
-              </Grid.Column>
-              <Grid.Column width={8} textAlign='right'>
-                <Card.Meta>
-                  <Icon name='map marker alternate' /> {voucher.neighborhood}
-                </Card.Meta>
-              </Grid.Column>
-            </Grid>
-          </Card.Content>
-          {/* </Link> */}
-        </Card>
-      ))}
+      {merged
+        .filter(item => item.display) // remove vouchers based on display flag.
+        .map(voucher => (
+          <Card link key={voucher.id} onClick={() => onClickHandler(voucher.product.metadata.slug)}>
+            {/* <Link to={`/exclusive-dining/${voucher.product.metadata.slug}`}> */}
+            <Img style={{ height: '200px' }} fluid={voucher.product.localFiles[0].childImageSharp.fluid} />
+            <Card.Content style={{ minHeight: '130px' }}>
+              <Label style={{ top: '-55px' }} color='blue' ribbon='right'>
+                Remaining: {checkRemaining(voucher.vouchersSold, voucher.quantity)}
+              </Label>
+              <Card.Header style={{ marginTop: '-30px', minHeight: '45px' }}>{voucher.name}</Card.Header>
+              <Card.Description style={{ minHeight: '38px' }}>{voucher.product.metadata.caption}</Card.Description>
+            </Card.Content>
+            <Card.Content>
+              <Grid>
+                <Grid.Column width={8}>
+                  <h3 style={{ color: 'black' }}>{formatCurrency(voucher.price)}</h3>
+                </Grid.Column>
+                <Grid.Column width={8} textAlign='right'>
+                  <Card.Meta>
+                    <Icon name='map marker alternate' /> {voucher.neighborhood}
+                  </Card.Meta>
+                </Grid.Column>
+              </Grid>
+            </Card.Content>
+            {/* </Link> */}
+          </Card>
+        ))}
     </Card.Group>
   )
 }
